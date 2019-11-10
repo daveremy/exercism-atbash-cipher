d9c53894cb15b75bad5259491f33ba2f1b6bbffe
Dave Remy <email@daveremy.com>
2019-11-10 16:29:19 +0100

    refactored to more functional approach

--- src/lib.rs
+++ src/lib.rs
@@ -2,48 +2,48 @@ const ALPHABET: [char; 26] = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
-const CHUNK_SIZE: u8 = 5;
+const SEPARATOR: &str = " ";
 
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    let mut encoded = String::new();
-    let mut chunk_char_count = 0;
-    for c in plain.chars() {
+    let mut count = 0;
+    plain.chars().fold(String::new(), |mut encoded, c| {
         match ALPHABET.iter().position(|&a| a == c.to_ascii_lowercase()) {
             Some(pos) => {
-                append_char(&mut encoded, ALPHABET[25 - pos], &mut chunk_char_count);
+                encoded.push_str(&format!("{}{}", separator(&mut count), ALPHABET[25 - pos]));
+                encoded
             }
-            None => {
-                if c.is_ascii() && !c.is_ascii_whitespace() && !c.is_ascii_punctuation() {
-                    append_char(&mut encoded, c, &mut chunk_char_count);
+            None => match c {
+                c if !c.is_ascii() || c.is_ascii_punctuation() || c.is_ascii_whitespace() => {
+                    encoded
                 }
-            }
+                _ => {
+                    encoded.push_str(&format!("{}{}", separator(&mut count), c));
+                    encoded
+                }
+            },
         }
-    }
-    encoded
+    })
 }
 
-fn append_char(encoded: &mut String, c: char, chunk_char_count: &mut u8) {
-    if *chunk_char_count == CHUNK_SIZE {
-        encoded.push(' ');
-        *chunk_char_count = 0 as u8;
+fn separator(count: &mut usize) -> &'static str {
+    *count += 1;
+    if *count == 6 {
+        *count = 1;
+        SEPARATOR
+    } else {
+        ""
     }
-    encoded.push(c);
-    *chunk_char_count += 1;
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    let mut decoded = String::new();
-    for c in cipher.chars() {
-        match ALPHABET.iter().position(|&a| a == c) {
-            Some(pos) => decoded.push(ALPHABET[25 - pos]),
-            None => {
-                if !c.is_ascii_whitespace() {
-                    decoded.push(c);
-                }
-            }
-        }
-    }
-    decoded
+    cipher
+        .chars()
+        .filter(|c| !c.is_ascii_whitespace())
+        .map(|c| match ALPHABET.iter().position(|&a| a == c) {
+            Some(pos) => ALPHABET[25 - pos],
+            None => c,
+        })
+        .collect::<String>()
 }
